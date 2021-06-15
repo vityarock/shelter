55db71e753dd7d6e830c58c3dba12acb409dd9e5
user <user@ivc1-m9.m9.maxidom.ru>
2021-06-15 22:49:20 +0300

    files for heroku

--- shelter/shelter/settings.py
+++ shelter/shelter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1ip-xb8%e_mu@q%_l#m8)pql0j5afme3&@!a%maiy07s&my)e+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
