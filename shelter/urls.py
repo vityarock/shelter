f0e7be8a7e159520a642f164a209e87347ea3ea3
vityarock <57987552+vityarock@users.noreply.github.com>
2021-07-24 12:18:46 +0300

    Update urls.py

--- shelter/urls.py
+++ shelter/urls.py
@@ -9,7 +9,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', index),
+    path('', PetView.as_view()),
     path('filter/<str:pk>/', PetViewDetail.as_view()),
     path('about/', AboutView.as_view()),
     path(
