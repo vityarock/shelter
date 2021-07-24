3a30260fb6e95598c279e5fe4a09ae5c507f4587
vityarock <57987552+vityarock@users.noreply.github.com>
2021-07-24 11:18:55 +0300

    Update urls.py

--- shelter/urls.py
+++ shelter/urls.py
@@ -1,18 +1,3 @@
-"""shelter URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
@@ -23,6 +8,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', AboutView.as_view()),
     path('filter/<str:pk>/', PetViewDetail.as_view()),
     path('about/', AboutView.as_view()),
