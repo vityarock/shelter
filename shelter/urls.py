ace07835547edd0903783116d0a0a45ff07fde8f
vityarock <57987552+vityarock@users.noreply.github.com>
2021-07-24 11:22:20 +0300

    Update urls.py

--- shelter/urls.py
+++ shelter/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
-from shelter_app.views import PetView, PetViewDetail, AboutView, FilteredView
+from shelter_app.views import PetView, PetViewDetail, AboutView, FilteredView, index
 from django.views.generic import RedirectView
 
 urlpatterns = [
